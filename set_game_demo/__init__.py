1536d9c5b27783ecce572b6821b252fcadc65009
Ryan Parman <ryan@ryanparman.com>
2016-09-24 17:49:49 -0700

    Wrote the initial code which sets up the deck and the board, and can deal cards. Added tests which validate the properties of the cards. Added some dependencies that were missed before, and updated the tox.ini definition file.

--- set_game_demo/__init__.py
+++ set_game_demo/__init__.py
@@ -1,3 +1,4 @@
+#! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
 """
@@ -9,10 +10,13 @@ Copyright (c) 2016 [Ryan Parman](https://github.com/skyzyx).
 """
 
 from __future__ import print_function
+import collections
+import random
 import six
+from prettytable import PrettyTable
 
 
-class Set(object):
+class SetGame(object):
     """
     "Set" is a card game where a group of players try to identify a "set" of cards from those placed face-up on a table.
 
@@ -29,11 +33,15 @@ class Set(object):
 
     For example:
 
-    * The cards "two red solid squiggles", "one green solid diamond", "three purple solid ovals" would make up a set. (number, shape, and color are different, shading is the same)
-    * The cards "two red solid squiggles", "one green solid squiggles", "three purple solid ovals" would not make up a set, because shape is the same on two cards, but different on the third.
-    * A game of Set starts by dealing 12 cards, face-up. When a player sees three cards that make up a set, they yell "Set!" and grab the cards. New cards are dealt from the deck to replace them.
-    * If no player can find a set, three more cards are dealt (to make 15, then 18, then 21…)
-    * The game is over when there are no cards left in the deck, and no sets left on the table. The player with the most sets wins.
+    * The cards "two red solid squiggles", "one green solid diamond", "three purple solid ovals" would make up a set.
+      (number, shape, and color are different, shading is the same)
+    * The cards "two red solid squiggles", "one green solid squiggles", "three purple solid ovals" would not make up a
+      set, because shape is the same on two cards, but different on the third.
+    * A game of Set starts by dealing 12 cards, face-up. When a player sees three cards that make up a set, they yell
+      "Set!" and grab the cards. New cards are dealt from the deck to replace them.
+    * If no player can find a set, three more cards are dealt (to make 15, then 18, then 21...)
+    * The game is over when there are no cards left in the deck, and no sets left on the table. The player with the most
+      sets wins.
 
     **Game Requirements:**
 
@@ -41,7 +49,8 @@ class Set(object):
 
     * A method that takes three cards, and determines whether the three cards make a set
     * A method that given a "board" of cards, will either find a set, or determine if there are no sets on the table
-    * A method that will play an entire game of Set, from beginning to end, and return a list of each valid sets you removed from the board.
+    * A method that will play an entire game of Set, from beginning to end, and return a list of each valid sets you
+      removed from the board.
 
     For this last method, there will be multiple correct solutions, but any valid list of sets is fine.
 
@@ -53,12 +62,98 @@ class Set(object):
     * Python 3.3
     * Python 3.4
     * Python 3.5
-    * Python 3.6 (dev)
+    * Python 3.6 (beta)
     * Pypy (~2.7.10)
     * Pypy3 (~3.2.5)
 
     **Import:**
 
-        from set_game_demo import Set
+        from set_game_demo import SetGame
 
     """
+
+    def __init__(self):
+        """
+        Constructs a new instance of this class.
+        """
+
+        self.colors = ["red", "green", "purple"]
+        self.shapes = ["diamond", "squiggle", "oval"]
+        self.shadings = ["solid", "empty", "striped"]
+        self.numbers = ["one", "two", "three"]
+
+        self.deck = []
+
+        for color in self.colors:
+            for shape in self.shapes:
+                for shading in self.shadings:
+                    for number in self.numbers:
+                        card = SimpleNamespace()
+                        card.color = color
+                        card.shape = shape
+                        card.shading = shading
+                        card.number = number
+                        self.deck.append(card)
+
+        random.shuffle(self.deck)
+        self.deck = collections.deque(self.deck)
+        self.board = []
+
+
+    def deal(self, cards=12):
+        """
+        Deals a given number of cards from the top (front) of the deck. The default number of cards dealt is `12`.
+        """
+
+        out = []
+
+        for i in six.moves.range(cards):
+            out.append(self.deck.popleft())
+
+        return out
+
+
+    def is_a_set(card1, card2, card3):
+        """
+        Determines whether a group of 3 cards is a _set_ or not. A value of `True` means that the group is a set. A
+        value of `False` means that the group is _not_ a set.
+        """
+
+
+
+    def display_cards(self, cards):
+        """
+        Accepts an array of cards, and renders them for humans in a table.
+        """
+
+        t = PrettyTable(['Number', 'Color', 'Shape', 'Shading'])
+
+        for card in cards:
+            t.add_row([card.number, card.color, card.shape, card.shading])
+
+        print(t)
+
+
+# ------------------------------------------------------------------------------
+
+class SimpleNamespace(object):
+    """
+    Python doesn't have the same concept as "objects" or "hashes" like Ruby, PHP, JavaScript, Java, and other
+    languages do. The closest thing we have is a `dict`, but it doesn't quite behave the same way.
+
+    Backported from Python 3.3+.
+
+    Small performance penalty for using this in Python 3.3+ because the original implementation was written in C. We
+    will trade a small bit of performance for convenience in version 1, and flag this as a known issue to resolve in a
+    future version.
+    """
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
+
+    def __repr__(self):
+        keys = sorted(self.__dict__)
+        items = ("{}={!r}".format(k, self.__dict__[k]) for k in keys)
+        return "{}({})".format(type(self).__name__, ", ".join(items))
+
+    def __eq__(self, other):
+        return self.__dict__ == other.__dict__
--- tests/test_runner.py
+++ tests/test_runner.py
@@ -12,15 +12,57 @@ Copyright (c) 2016 [Ryan Parman](https://github.com/skyzyx).
 from __future__ import print_function
 import unittest
 import nose2
-from set_game_demo import Set
+from nose2.tools.such import helper
+from set_game_demo import SetGame
 
 class Test(unittest.TestCase):
-    """Unit tests for the set_game_demo.Set class."""
+    """Unit tests for the set_game_demo.SetGame class."""
 
     def setUp(self):
         """Instantiate the class."""
+        self.set = SetGame()
 
-    def test_first(self):
+    def test_colors(self):
+        self.assertEqual(True, "red" in self.set.colors)
+        self.assertEqual(True, "green" in self.set.colors)
+        self.assertEqual(True, "purple" in self.set.colors)
+        self.assertEqual(False, "blue" in self.set.colors)
+        self.assertEqual(False, "fuschia" in self.set.colors)
+        self.assertEqual(False, "yellow" in self.set.colors)
+
+    def test_shapes(self):
+        self.assertEqual(True, "diamond" in self.set.shapes)
+        self.assertEqual(True, "squiggle" in self.set.shapes)
+        self.assertEqual(True, "oval" in self.set.shapes)
+        self.assertEqual(False, "hearts" in self.set.shapes)
+        self.assertEqual(False, "clubs" in self.set.shapes)
+        self.assertEqual(False, "spades" in self.set.shapes)
+
+    def test_shadings(self):
+        self.assertEqual(True, "solid" in self.set.shadings)
+        self.assertEqual(True, "empty" in self.set.shadings)
+        self.assertEqual(True, "striped" in self.set.shadings)
+        self.assertEqual(False, "spotted" in self.set.shadings)
+        self.assertEqual(False, "dotted" in self.set.shadings)
+        self.assertEqual(False, "criss-cross" in self.set.shadings)
+
+    def test_numbers(self):
+        self.assertEqual(True, "one" in self.set.numbers)
+        self.assertEqual(True, "two" in self.set.numbers)
+        self.assertEqual(True, "three" in self.set.numbers)
+        self.assertEqual(False, "pi" in self.set.numbers)
+        self.assertEqual(False, "four" in self.set.numbers)
+        self.assertEqual(False, "five" in self.set.numbers)
+
+    def test_deck(self):
+        self.assertEqual(81, len(self.set.deck)) # Should be 3^4 (not 4^3)
+
+    def test_deal(self):
+        self.assertEqual(3, len(self.set.deal(3)))
+        self.assertEqual(6, len(self.set.deal(6)))
+        self.assertEqual(12, len(self.set.deal(12)))
+        self.assertEqual(12, len(self.set.deal()))
+        # helper.assertRaises(IndexError, self.set.deal(48)) # Not quite working. 81 - 3 - 6 - 12 - 12 = 48
 
 if __name__ == '__main__':
     nose2.main()
